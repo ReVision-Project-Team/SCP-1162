347cc9d752ce016ca4772b11b67b1abbefc75c5c
Phil Timersky <96748043+Time2138@users.noreply.github.com>
2023-11-27 23:39:42 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SCP-1162")]
+[assembly: AssemblyCompany("Timersky")]
+[assembly: AssemblyProduct("SCP 1162")]
+[assembly: AssemblyCopyright("Copyright © Timersky 2023")]
+[assembly: AssemblyTrademark("Timersky")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07dac62c-0736-49f3-99ee-1d62eeb437c1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
