f5c15e8ad0e5825c33df83093e1a523d2d97974e
Phil Timersky <96748043+Time2138@users.noreply.github.com>
2023-11-28 10:21:55 +0300

    add spawn

--- Handlers/ServerHandlers.cs
+++ Handlers/ServerHandlers.cs
@@ -1,10 +1,26 @@
-﻿namespace SCP1162.Handlers
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using System.Linq;
+using UnityEngine;
+
+namespace SCP1162.Handlers
 {
     internal sealed class ServerHandlers
     {
         public void OnRoundStarted()
         {
-
+            Room _room = Room.List.FirstOrDefault(x => x.Type == Exiled.API.Enums.RoomType.Lcz173);
+            GameObject scp1162 = Item.Create(ItemType.SCP500).CreatePickup(Vector3.zero).GameObject;
+            scp1162.GetComponent<Rigidbody>().useGravity = false;
+            scp1162.GetComponent<Rigidbody>().drag = 0f;
+            scp1162.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+            scp1162.GetComponent<Rigidbody>().freezeRotation = true;
+            scp1162.GetComponent<Rigidbody>().detectCollisions = false;
+            scp1162.transform.parent = _room.transform;
+            scp1162.transform.localPosition = new Vector3(16.608f, 12.966f, 3.761f);
+            scp1162.transform.localRotation = Quaternion.Euler(90, 0, 0);
+            scp1162.transform.localScale = new Vector3(5, 5, 5);
+            scp1162.SetActive(true);
         }
     }
 }
